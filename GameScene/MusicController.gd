8886b412e80160f24258a12f8370bbafc9759eef
DavidBystroem <a18davby@student.his.se>
2022-07-17 19:39:09 +0200

    theme music

---
+++ GameScene/MusicController.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var game = load("res://Assets/Sound/Dice_game_bit_1_loop.ogg")
+
+func _ready():
+	pass
+	
+func playgame():
+	$Music.stream = game
+	$Music.play()
--- UI/Menues/StartMenu.gd
+++ UI/Menues/StartMenu.gd
@@ -3,9 +3,13 @@ extends Control
 onready var startbtn = $MenuItems/Start
 onready var Buttonsound = $Buttonsound
 
+
+
 func _ready():
 	startbtn.grab_focus()
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	MusicController.playgame()
 	
 func _on_Start_pressed():
 	get_tree().change_scene("res://GameScene/GameWorld.tscn")
