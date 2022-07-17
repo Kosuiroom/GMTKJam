ea539b7eba01b7b54fc1bc0b7084f66a068d127b
Pontus <pontus.g91@gmail.com>
2022-07-17 15:03:58 +0200

    working on camera

--- GameScene/GameWorld.gd
+++ GameScene/GameWorld.gd
@@ -1,10 +1,7 @@
 extends Node2D
 class_name GameWorld
 
-func SpawnPlayer() -> void:
-	player.position = Vector2(200,55)
-	platform.position = Vector2(200,90)
-	set_camera_limits()
+onready var _tile_map = $TileMap
 
 func set_camera_limits():
 	var map_limits = _tile_map.get_used_rect()
@@ -12,3 +9,6 @@ func set_camera_limits():
 	$Player/Camera2D.limit_left = map_limits.position.x * map_cellsize.x
 	$Player/Camera2D.limit_right = map_limits.end.x * map_cellsize.x
 	$Player/Camera2D.limit_top = map_limits.position.y * map_cellsize.y
+
+func ready():
+	set_camera_limits()
